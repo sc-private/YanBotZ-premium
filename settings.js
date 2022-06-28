185618f951c2d455b378c1ecd78e548e700557d4
YannExecuted <iyanryan3@gmail.com>
2022-06-28 12:11:53 +0800

    YanBotZ-premium

---
+++ settings.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.APIs = {
+	webnya: 'https://api.lolhuman.xyz',
+}
+
+global.APIKeys = {
+	'https://api.lolhuman.xyz': 'RINTISAJA',
+}
+
+global.owner = ['6282252285143']
+global.premium = ['6282252285143']
+global.packname = 'YannBotZ - Premium'
+global.author = 'Wa : https://wa.me/6282252285143\nYT :gk ada yt×\nIG : ryannn.store\nGH : gk tau\nTT : gk tau'
+global.sessionName = 'kirbotz'
+global.namabotnya = 'YannBotZ'
+global.namaownernya = 'Owner YannBotZ'
+global.lolkey = 'RINTISAJA'
+global.wame = 'https://wa.me/6282252285143'
+global.gckirbotz = 'https://chat.whatsapp.com/BvwLZPkx6rm26ZGzNtPApi'
+global.ownerNumber = ["6282252285143@s.whatsapp.net"]
+global.email = 'ryandevelopment@gmail.com'
+global.yt = '-'
+global.region = 'Indonesia'
+global.prefa = ['','!','.','#','-','•']
+global.sp = '⭔'
+global.mess = {
+    success: 'Success 💯',
+    admin: 'Fitur Khusus Admin Group!!!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!!!',
+    owner: 'Fitur Khusus Owner Bot!!!',
+    group: 'Fitur Digunakan Hanya Untuk Group!!!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!!!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot!!!',
+    error: 'Error Kak Silahkan Chat Owner',
+    wait: '❗Waittt...',
+    noPetualang: 'Itssss Kamu Belum Jadi Member Petualang, Silahkan Ketik .joinrpg <namamu>',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    premium: 'Maaf Sebelumya Kamu Belum Premium, Silahkan Pencet Di Bawah Untuk Beli Premium',
+    banned: 'Maaf Kamu Sudah Di Banned Silahkan Chat Owner Untuk Membuka Banned Nya',
+}
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 5
+}
+
+
+global.thumb = fs.readFileSync('./Image/kirbotz.jpg')
+global.thumbrpg = fs.readFileSync('./Image/kirbotzrpg.jpg')
+global.imgjoinrpg = fs.readFileSync('./Image/joinrpg.jpg')
+global.imgmining = fs.readFileSync('./Image/mining.jpg')
+
+global.imgdevil = fs.readFileSync('./Image/img/devil.jpg')
+global.imgslime = fs.readFileSync('./Image/img/slime.jpg')
+global.imgdemon = fs.readFileSync('./Image/img/demon.jpg')
+global.imggoblin = fs.readFileSync('./Image/img/goblin.jpg')
+global.imgdemonking = fs.readFileSync('./Image/img/demonking.jpg')
+global.imgbehemoth = fs.readFileSync('./Image/img/behemoth.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
